fa7cf99f5a5230d6c258d5b79643034467050492
Yellko Carvajal Mora <cincottel@gmail.com>
2019-10-30 00:55:01 -0600

    avance de descarga archivo

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,61 @@
 #!/usr/bin/env node
 
-console.log(process.argv[2]);
+const { exec } = require('child_process');
+
+/**
+ * 3. Create directory
+ */
+exec('mkdir public/repo', (err, stdout, stderr) => {
+    if (err) {
+        console.error(err)
+    } else {
+
+        console.log("paso 1");
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    }
+});
+
+/**
+ * 1. Download repository
+ */
+
+exec(`git clone ${process.argv[2]} ./public/repo`, (err, stdout, stderr) => {
+    if (err) {
+        console.error(err)
+    } else {
+        console.log("paso 2");
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    }
+});
+
+/**
+ * 2. Create log file
+ */
+
+let command = 'git --git-dir=public/repo/.git log --stat >> public/log.txt'
+
+exec(command, (err, stdout, stderr) => {
+    if (err) {
+        console.error(err)
+    } else {
+        console.log("paso 3");
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    }
+});
+
+/**
+ * 3. Delete remote branch
+ */
+exec('rm -Rf public/repo', (err, stdout, stderr) => {
+    if (err) {
+        console.error(err)
+    } else {
+        console.log("paso 4");
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    }
+});
+
